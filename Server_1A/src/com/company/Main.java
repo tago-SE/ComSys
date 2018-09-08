cc27816cacb1af86e3c5871cba6742f9376aa1e6
Globey163 <globey163@gmail.com>
2018-09-08 15:38:30 +0200

    argument failsafe on server

--- Server_1A/src/com/company/Main.java
+++ Server_1A/src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        if (args.length != 1 || !args[0].matches("[a-zA-Z]+")){
+            System.err.println("Argument error!");
+            System.exit(1);
+        }
     }
 }
