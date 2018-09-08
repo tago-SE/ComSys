25196637a1e8def5955d2ea5804d1081c8eb0ecc
tago-SE <tiagor@kth.se>
2018-09-08 16:18:47 +0200

    client stuff

--- Client_1A/src/com/company/Main.java
+++ Client_1A/src/com/company/Main.java
@@ -1,9 +1,34 @@
 package com.company;
 
+import java.io.IOException;
+import java.net.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Testing commit");
-        System.out.println("yea, fuck you buddy");
+    public static void main(String[] args) throws IOException {
+        // get a datagram socket
+        DatagramSocket socket = null;
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException e1) {
+            e1.printStackTrace();
+        }
+        // send request
+        byte[] buffer = new byte[256];
+        InetAddress address = InetAddress.getByName("localhost");
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
+
+        socket.send(packet);
+
+
+        // get response
+        packet = new DatagramPacket(buffer, buffer.length);
+        socket.receive(packet);
+
+        // display response
+        String received = new String(packet.getData(), 0, packet.getLength());
+        System.out.println("Quote of the Moment: " + received);
+
+        socket.close();
     }
 }
