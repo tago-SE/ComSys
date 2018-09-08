64ad0caa2e79835ae8e8363bd2d44ace915a522a
Globey163 <globey163@gmail.com>
2018-09-08 18:44:43 +0200

    Initiated communication.

--- Client_1A/src/com/company/Main.java
+++ Client_1A/src/com/company/Main.java
@@ -2,11 +2,15 @@ package com.company;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("testing with gitignore");
+
+        Scanner scan = new Scanner(System.in);
+        String sendHello = null;
+
         // get a datagram socket
         DatagramSocket socket = null;
         try {
@@ -14,17 +18,30 @@ public class Main {
         } catch (SocketException e1) {
             e1.printStackTrace();
         }
+
+        socket.setSoTimeout(3000);
         // send request
-        byte[] buffer = new byte[256];
+        sendHello = scan.nextLine();
+        byte[] buffer = sendHello.getBytes();
         InetAddress address = InetAddress.getByName("localhost");
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
 
         socket.send(packet);
 
-
         // get response
-        packet = new DatagramPacket(buffer, buffer.length);
-        socket.receive(packet);
+        byte[] response = new byte[4096];
+        packet = new DatagramPacket(response, response.length);
+
+
+        try{
+            socket.receive(packet);
+        }
+        catch(SocketTimeoutException stoEx){
+
+            System.out.println("Server connection timed out, exiting.");
+            System.exit(1);
+        }
+
 
         // display response
         String received = new String(packet.getData(), 0, packet.getLength());
