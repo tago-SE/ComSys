672e189d6e8977580c97e22edfb7a6531c193636
tago-SE <tiagor@kth.se>
2018-09-08 16:17:12 +0200

    Server stuff

--- Server_1A/src/com/company/Main.java
+++ Server_1A/src/com/company/Main.java
@@ -1,8 +1,95 @@
 package com.company;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.io.*;
+import java.net.*;
+import java.util.Date;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    static class Host {
+        int port;
+        InetAddress address;
+
+        public Host(InetAddress address, int port) {
+            this.address = address;
+            this.port = port;
+        }
+
+        public String toString() {
+            return address.getHostAddress() + ":" + port;
+        }
     }
+
+    /*
+    static void response(Socket socket, String arg){
+        String sendString = "polo";
+        byte[] sendData = new byte[0];
+        try {
+            sendData = sendString.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
+        socket.send(sendPacket);
+    }
+    */
+
+    static void response(Socket socket, Host host, String s) {
+
+    }
+
+    public static void main(String[] args) {
+        Host server = null;
+        DatagramSocket socket = null;
+        try {
+            server =  new Host(InetAddress.getLocalHost(), 4445);
+            socket = new DatagramSocket(server.port);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            return;
+        } catch (SocketException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        byte[] buffer = new byte[256];
+        DatagramPacket recievePacket = new DatagramPacket(buffer, buffer.length);
+
+        boolean run = true;
+        System.out.println("Listening on udp:" + server);
+
+        Host client = null;
+
+        while (run) {
+            try {
+                socket.receive(recievePacket);
+                Host newHost = new Host(recievePacket.getAddress(), recievePacket.getPort());
+                // Block multiple connections
+                if (client != null && newHost.address != client.address) {
+
+                }
+                client = newHost;
+
+
+                String recv = new String(recievePacket.getData(), 0, recievePacket.getLength());
+                System.out.println("RECEIVED: " + recv);
+
+                String send = "polo";
+                byte[] sendData = send.getBytes("UTF-8");
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, client.address, client.port);
+                socket.send(sendPacket);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+               run = false;
+            }
+        }
+        if (socket != null)
+            socket.close();
+
+    }
+
 }
