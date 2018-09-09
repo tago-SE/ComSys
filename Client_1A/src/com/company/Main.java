865ad8993d5651119d2286d477b9261f9402196a
Globey163 <globey163@gmail.com>
2018-09-09 17:21:40 +0200

    Client now more compliant with the "A Solid Network Application" guidelines.

--- Client_1A/src/com/company/Main.java
+++ Client_1A/src/com/company/Main.java
@@ -9,44 +9,57 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         Scanner scan = new Scanner(System.in);
-        String sendHello = null;
+        String sendHello, receivedResponse, gameInput;
+        boolean run;
+        DatagramSocket socket = null;
 
         // get a datagram socket
-        DatagramSocket socket = null;
         try {
             socket = new DatagramSocket();
         } catch (SocketException e1) {
             e1.printStackTrace();
-        }
-
-        socket.setSoTimeout(3000);
-        // send request
-        sendHello = scan.nextLine();
-        byte[] buffer = sendHello.getBytes();
-        InetAddress address = InetAddress.getByName("localhost");
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
-
-        socket.send(packet);
-
-        // get response
-        byte[] response = new byte[4096];
-        packet = new DatagramPacket(response, response.length);
-
-
-        try{
-            socket.receive(packet);
-        }
-        catch(SocketTimeoutException stoEx){
-
-            System.out.println("Server connection timed out, exiting.");
             System.exit(1);
         }
 
+        try {
 
-        // display response
-        String received = new String(packet.getData(), 0, packet.getLength());
-        System.out.println("Quote of the Moment: " + received);
+            socket.setSoTimeout(3000);
+            // send request
+            sendHello = scan.nextLine();
+            byte[] buffer = sendHello.getBytes();
+            InetAddress address = InetAddress.getByName("localhost");
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 4445);
 
-        socket.close();
+            socket.send(packet);
+
+            // get response
+            byte[] response = new byte[4096];
+            packet = new DatagramPacket(response, response.length);
+
+
+                socket.receive(packet);
+
+            receivedResponse = new String(packet.getData(), 0, packet.getLength());
+            if (receivedResponse.equals("OK")) {
+                run = true;
+                System.out.println("Run set to true");
+            } else if (receivedResponse.equals("BUSY")) {
+                System.exit(1);
+            }
+
+        } catch (IOException ioExc){
+
+            ioExc.printStackTrace();
+            System.exit(1);
+        }
+        finally{
+
+            try{
+                socket.close();
+            } catch (Exception ex){
+                ex.printStackTrace();
+                System.exit(1);
+            }
+        }
     }
 }
