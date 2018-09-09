c153c3a85882840f8c73899e4381eba7ea2cbb8b
Globey163 <globey163@gmail.com>
2018-09-09 17:12:50 +0200

    Tiago's work, commited through me.

--- Server_1A/src/com/company/Main.java
+++ Server_1A/src/com/company/Main.java
@@ -1,105 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.company;
-
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 
 import java.io.*;
 import java.net.*;
-import java.util.Date;
 
 public class Main {
 
     static class Host {
         int port;
         InetAddress address;
+        long time;
 
         public Host(InetAddress address, int port) {
             this.address = address;
             this.port = port;
-        }
-
-        public String toString() {
-            return address.getHostAddress() + ":" + port;
+            this.time = System.currentTimeMillis();
         }
     }
 
-    /*
-    static void response(Socket socket, String arg){
-        String sendString = "polo";
-        byte[] sendData = new byte[0];
-        try {
-            sendData = sendString.getBytes("UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, clientAddress, clientPort);
-        socket.send(sendPacket);
-    }
-    */
-
-    public static DatagramPacket CreatePacket(Host client, String msg) throws UnsupportedEncodingException {
+    public static DatagramPacket createPacket(InetAddress ip, int port, String msg) throws UnsupportedEncodingException {
         byte[] data = msg.getBytes("UTF-8");
-        return new DatagramPacket(data, data.length, client.address, client.port);
+        return new DatagramPacket(data, data.length,ip, port);
     }
+
+
 
     public static void main(String[] args) {
-
-        System.out.println("test push with working gitignore");
         if (args.length != 1 || !args[0].matches("[a-zA-Z]+")){
-            System.err.println("Argument error!");
+            (new IllegalArgumentException()).printStackTrace();
+            System.exit(1);
+        }
+        DatagramSocket socket = null;
+        try {
+            socket = new DatagramSocket(4445);
+        } catch (SocketException e) {
             System.exit(1);
         }
 
-
-        Host server = null;
-        DatagramSocket socket = null;
-        try {
-            server =  new Host(InetAddress.getLocalHost(), 4445);
-            socket = new DatagramSocket(server.port);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-            return;
-        } catch (SocketException e) {
-            e.printStackTrace();
-            return;
-        }
+        System.out.println("Listening on udp:" + socket.getLocalAddress() + ":" + socket.getPort());
 
         byte[] buffer = new byte[256];
-        DatagramPacket recievePacket = new DatagramPacket(buffer, buffer.length);
-
+        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
         boolean run = true;
-        System.out.println("Listening on udp:" + server);
+        Host curr;
+        Host prev = null;
+        int guessCount = 0;
+        int guessMax = args[0].length();
+        String secret = args[0];
+        boolean waitingForHello = true;
 
-        Host client = null;
+        try {
+            while (run) {
 
-        while (run) {
-            try {
-                socket.receive(recievePacket);
-                Host newHost = new Host(recievePacket.getAddress(), recievePacket.getPort());
-
-                // Block multiple connections
-                if (client != null && newHost.address != client.address) {
-                    socket.send(CreatePacket(newHost, "Server is already in use."));
-                }
+                socket.receive(packet);
 
 
-                client = newHost;
-                String recv = new String(recievePacket.getData(), 0, recievePacket.getLength());
-                System.out.println("RECEIVED: " + recv);
+                // Manage connections - one client at a time
 
+                curr = new Host(packet.getAddress(), packet.getPort());
 
-                socket.send(CreatePacket(client, "polo"));
+                if (prev != null && prev.address != curr.address) {
 
+                    if (curr.time < prev.time + 10.000) {
 
+                        socket.send(createPacket(curr.address, curr.port, "BUSY"));
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-               run = false;
+                        continue; // Ignore client
+                        } else {
+                            // Terminate previous session if a new connection is
+                            // accepted and the timer has expired
+                            socket.send(createPacket(prev.address, prev.port, "ERROR 1"));
+                            waitingForHello = true;
+                        }
+                    }
+
+                    String recv = new String(packet.getData(), 0, packet.getLength());
+
+                    if (waitingForHello) {
+                        if (recv.equals("HELLO")) {
+                            waitingForHello = false;
+                            socket.send(createPacket(curr.address, curr.port, "OK"));
+                        } else {
+                            socket.send(createPacket(curr.address, curr.port, "ERROR 2"));
+                        }
+                    } else {
+                        if (recv.equals("START")) {
+                            socket.send(createPacket(curr.address, curr.port, "READY" + secret.length()));
+                            guessCount = 0;
+                        } else if (recv.substring(0, 5).equals("GUESS")) {
+                            System.out.println("GUESS RECIEVED");
+                        } else {
+                            socket.send(createPacket(curr.address, curr.port, "ERROR 3"));
+                        }
+                    }
+                    prev = curr;
             }
         }
-        if (socket != null)
-            socket.close();
+        catch (IOException e) {
+            e.printStackTrace();
+            run = false;
+        }
+        finally{
 
+            try{
+                socket.close();
+            }catch (Exception ex){
+                ex.printStackTrace();
+                System.exit(1);
+            }
+        }
     }
+
 
 }
