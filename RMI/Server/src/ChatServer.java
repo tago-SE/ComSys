515cfc80ed19d0be825cf740090dd25a71302b96
tago-SE <tiagor@kth.se>
2018-09-14 16:18:28 +0200

    a

--- RMI/Server/src/ChatServer.java
+++ RMI/Server/src/ChatServer.java
@@ -1,4 +1,4 @@
-import RMI.ChatClientInt;
+ import RMI.ChatClientInt;
 import RMI.ChatServerInt;
 
 import java.rmi.*;
@@ -7,55 +7,84 @@ import java.util.*;
 
 public class ChatServer  extends UnicastRemoteObject implements ChatServerInt {
 
-    private Vector v=new Vector();
-    public ChatServer() throws RemoteException{}
+    private List<ChatClientInt> clients;
+    private int counter = 0;
+    private static final String DEFAULT_NAME = "Unknown";
+
+    public ChatServer() throws RemoteException{
+        clients = Collections.synchronizedList(new ArrayList<>());
+    }
 
     @Override
-    public void message(ChatClientInt client, String msg) throws RemoteException {
+    public synchronized void message(ChatClientInt client, String msg) {
         System.out.println("Message: " + msg);
-        client.response("Response: " + msg);
-    }
-
-    @Override
-    public void command(String cmd) throws RemoteException {
-        System.out.println("Command: " + cmd);
-    }
-
-    @Override
-    public void connect(ChatClientInt client) throws RemoteException {
-        System.out.println(client + " got connected...");
-        // Set name
-        client.response("Welcome to the RMI Chat Server!");
-    }
-
-    @Override
-    public void disconnect(ChatClientInt client) throws RemoteException {
-        System.out.println(client + " got disconnected...");
-    }
-
-    public boolean login(ChatClientInt a) throws RemoteException{
-        System.out.println(a.getName() + "  got connected....");
-        a.tell("You have Connected successfully.");
-        publish(a.getName()+ " has just connected.");
-        v.add(a);
-        return true;
-    }
-
-    public void publish(String s) throws RemoteException{
-        System.out.println(s);
-        for(int i=0;i<v.size();i++){
-            try{
-                ChatClientInt tmp=(ChatClientInt)v.get(i);
-                tmp.tell(s);
-            }catch(Exception e){
-                //problem with the client not connected.
-                //Better to remove it
-            }
+        try {
+            client.response("Response: " + msg);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            clients.remove(client);
         }
     }
 
-    public Vector getConnected() throws RemoteException{
-        return v;
+    private synchronized boolean isNickAvailable(String nick) {
+        String lowerCaseNickname = nick.toLowerCase();
+        Iterator itr = clients.iterator();
+        while (itr.hasNext()) {
+            ChatClientInt c = (ChatClientInt) itr.next();
+            try {
+                if (c.getName().toLowerCase().equals(lowerCaseNickname)) {
+                    return false;
+                }
+            } catch (Exception e) {
+                clients.remove(c);
+            }
+        }
+        return true;
+    }
+
+    public synchronized void nickname(ChatClientInt client, String nick) throws RemoteException {
+        try {
+            if (isNickAvailable(nick)) {
+                client.setName(nick);
+                client.response("Nickname changed to: " + nick);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            clients.remove(c);
+        }
+    }
+
+    @Override
+    public synchronized void help(ChatClientInt client) throws RemoteException {
+        client.response ("Commands:\n" + "/quit\n" + "/users\n" +  "/whoami\n" + "/nick <nickname>\n" + "/help");
+    }
+
+    @Override
+    public synchronized void users(ChatClientInt client) throws RemoteException {
+        client.response ("Users...\nA...");
+    }
+
+    @Override
+    public void quit(ChatClientInt client)  throws RemoteException {
+        disconnect(client);
+    }
+
+    @Override
+    public synchronized void connect(ChatClientInt client) throws RemoteException {
+        System.out.println(client + " got connected...");
+        client.response("Welcome to the RMI Chat Server!");
+        client.setName(DEFAULT_NAME + " " + ++counter);
+        clients.add(client);
+    }
+
+    @Override
+    public synchronized void disconnect(ChatClientInt client) throws RemoteException {
+        // String name
+        System.out.println(client + " got disconnected...");
+        clients.remove(client);
+        for (ChatClientInt localClient : clients) {
+            localClient.response(client.getName() + " has disconnected");
+        }
     }
 
     public static void main(String[] args) {
@@ -63,11 +92,11 @@ public class ChatServer  extends UnicastRemoteObject implements ChatServerInt {
             //System.setSecurityManager(new RMISecurityManager());
             java.rmi.registry.LocateRegistry.createRegistry(1099);
 
-            ChatServerInt b=new ChatServer();
-            Naming.rebind("rmi://localhost/myabc", b);
-            System.out.println("[System] Chat Server is ready.");
+            ChatServerInt server = new ChatServer();
+            Naming.rebind("rmi://localhost/myabc", server);
+            System.out.println("Chat Server is ready.");
         }catch (Exception e) {
-            System.out.println("Chat Server failed: " + e);
+            System.out.println("Chat Server failed: " + e.getMessage());
         }
     }
 }
--- RMI/Server/src/RMI/ChatClientInt.java
+++ RMI/Server/src/RMI/ChatClientInt.java
@@ -4,8 +4,7 @@ import java.rmi.*;
 
 public interface ChatClientInt extends Remote{
 
-    public void response(String msg) throws RemoteException ;
-
-    public void tell (String name) throws RemoteException ;
-    public String getName()throws RemoteException ;
+    void response(String msg) throws RemoteException ;
+    String getName() throws RemoteException;
+    void setName(String name) throws RemoteException;
 }
