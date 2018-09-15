545f0a592b9001ca03deb9c8c24ab4c5315e7617
tago-SE <tiagor@kth.se>
2018-09-15 05:38:44 +0200

    unicast/multicast

--- RMI/Server/src/RMI/ChatClientInt.java
+++ RMI/Server/src/RMI/ChatClientInt.java
@@ -5,6 +5,4 @@ import java.rmi.*;
 public interface ChatClientInt extends Remote {
 
     void response(String msg) throws RemoteException ;
-    String getName() throws RemoteException;
-    void setName(String name) throws RemoteException;
 }
